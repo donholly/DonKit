93bd0f0fd101c83f0a8cf9e715483fbfdca91b95
Don Holly <don.holly@gmail.com>
2018-08-09 20:15:46 -0700

    Add completion handler to UIAlertController

--- DonKit/Classes/extensions/UIAlertController-extensions.swift
+++ DonKit/Classes/extensions/UIAlertController-extensions.swift
@@ -1,8 +1,10 @@
 
 extension UIAlertController {
-    public class func show(inViewController viewController: UIViewController?, title: String, message: String) {
+    public class func show(inViewController viewController: UIViewController?, title: String, message: String, completion: EmptyClosure? = nil) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: .default, handler: { action in
+            completion?()
+        }))
         (viewController ?? UIApplication.shared.topViewController)?.present(alert, animated: true, completion: nil)
     }
 }
