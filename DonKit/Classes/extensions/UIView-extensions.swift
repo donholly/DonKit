4624027bf4e3c1525b8132dc6dde9f83b4db5489
Don Holly <don.holly@gmail.com>
2018-10-28 19:18:04 -0700

    Add NibInstantiatable for UIView

--- DonKit/Classes/extensions/UIView-extensions.swift
+++ DonKit/Classes/extensions/UIView-extensions.swift
@@ -9,35 +9,28 @@
 import Foundation
 import UIKit
 
-extension UIView {
+public protocol NibInstantiatable {
+    static var nibName: String { get }
+    static var nibBundle: Bundle { get }
+    static var nibOwner: Any? { get }
+    static var nibOptions: [UINib.OptionsKey: Any]? { get }
+    static var instantiateIndex: Int { get }
+}
 
-    public func xibSetup() {
-        let view = loadFromNib()
-        addSubview(view)
-        setupContentView(view)
+public extension NibInstantiatable where Self: NSObject {
+    public static var nibName: String {
+        let className = String(describing: self)
+        return className
     }
-    
-    public func loadFromNib<T: UIView>() -> T {
-        let selfType = type(of: self)
-        let bundle = Bundle(for: selfType)
-        let nibName = String(describing: selfType)
-        let nib = UINib(nibName: nibName, bundle: bundle)
-        
-        guard let view = nib.instantiate(withOwner: self, options: nil).first as? T else {
-            fatalError("Error loading nib with name \(nibName)")
-        }
-        
-        return view
-    }
+    public static var nibBundle: Bundle { return Bundle(for: self) }
+    public static var nibOwner: Any? { return self }
+    public static var nibOptions: [UINib.OptionsKey: Any]? { return nil }
+    public static var instantiateIndex: Int { return 0 }
+}
 
-    private func setupContentView(_ view: UIView) {
-        view.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            view.topAnchor.constraint(equalTo: topAnchor),
-            view.leftAnchor.constraint(equalTo: leftAnchor),
-            view.rightAnchor.constraint(equalTo: rightAnchor),
-            view.bottomAnchor.constraint(equalTo: bottomAnchor)
-            ])
+public extension NibInstantiatable where Self: UIView {
+    public static func fromNib() -> Self {
+        let nib = UINib(nibName: nibName, bundle: nibBundle)
+        return nib.instantiate(withOwner: nibOwner, options: nibOptions)[instantiateIndex] as! Self
     }
 }
