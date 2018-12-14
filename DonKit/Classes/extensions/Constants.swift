bb6a882f252b03cced2330cef4c76f61623f617f
Don Holly <don.holly@gmail.com>
2018-12-13 23:03:41 -0800

    Add BoolCompletionClosure

--- DonKit/Classes/extensions/Constants.swift
+++ DonKit/Classes/extensions/Constants.swift
@@ -5,6 +5,7 @@
 //  Created by Don Holly on 11/1/17.
 //
 
+public typealias BoolCompletionClosure = ((_: Bool) -> Void)
 public typealias EmptyClosure = () -> Void
 public typealias ErrorableCompletionClosure = ((_ error: Error?) -> Void)
 public typealias ErrorableBoolCompletionClosure = ((_ success: Bool, _ error: Error?) -> Void)
